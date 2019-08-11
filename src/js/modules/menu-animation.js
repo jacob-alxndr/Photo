7a07babefe066f57e68f3eb0350880067e1e8e6f
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-10 19:31:18 -0700

    fixed menu animation

--- src/js/modules/menu-animation.js
+++ src/js/modules/menu-animation.js
@@ -1,16 +1,15 @@
 // 1. Selectors
 import { elements } from "./DOMelements";
-import { delay } from "q";
 
 //2. Actions
 const fadeUp = function () {
-    let time = 15
+    let time = 0
     elements.menuItems.forEach(item =>{
-        time = time - 1
+        time = time + .05
         item.style.opacity = 1;
+   
+        item.style.transition = `all 1.2s ${time}s`;
         item.style.transform = `translateY(0)`;
-        item.style.transition = `all 3s`;
-        item.style.transitionDelay = `${time}s`;
       
         console.log(item)
     })
@@ -18,4 +17,4 @@ const fadeUp = function () {
 }
 
 // 3. Events
-document.addEventListener('load', fadeUp());
+window.addEventListener('load', fadeUp);
