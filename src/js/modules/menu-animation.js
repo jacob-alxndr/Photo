557296a5bb4d44448cdd5e37018a4fd4c8bec3cf
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-12 10:50:30 -0700

    Adding more images

--- src/js/modules/menu-animation.js
+++ src/js/modules/menu-animation.js
@@ -6,7 +6,7 @@ const fadeUp = function () {
     elements.menuItems.forEach(item =>{
         time = time + .05
         item.style.opacity = 1;
-        item.style.transition = `opacity 1.2s ${time}s, transform 1.2s ${time}s, color .3s`;
+        item.style.transition = `opacity 1.2s ${time}s, transform 1.2s ${time}s, color .3s, margin .1s`;
         item.style.transform = `translateY(0)`;
     })
 
