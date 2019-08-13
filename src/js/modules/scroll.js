b5fbd474c16334c18d18e98fc21134917ab6adbe
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-13 13:57:54 -0700

    added ijji html

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -3,28 +3,41 @@ import { elements } from "./DOMelements";
 
 //2.Actions
 if (elements.gallerySet){
+
     const images = elements.gallerySet.querySelectorAll('.image-set')
+
+    images.forEach(image =>{
+        image.style.opacity = 0;
+    })
+    
     const scroll = function () {
-        let delay = 0.25
+        let odd = 2
+        let delay = 0.5
         images.forEach(image =>{
             const topTag = image.getBoundingClientRect().top
             const bottomTag = image.getBoundingClientRect().bottom
-            console.log(topTag)
-            if(topTag < window.innerHeight & bottomTag > 0){
-                image.style.animation = `fadein 3s ${delay}s both`
-                delay = delay + 0.2
-                image.style.border = "5px solid green"
+
+            if(topTag < window.innerHeight && bottomTag > 0){
+                image.style.animation = `fadeIn 1s ${delay}s both`
+                odd = odd + 1
+                if(odd%2 == 0){
+                    delay = delay + 0.5
+                }
+                console.log(delay)
+                image.style.opacity = 1;
+                // image.style.border = "5px solid green"
             }
             else{
                 image.style.opacity = 0
                 image.style.animation = ""
-                image.style.border = "5px solid red"
             }
         })
        
     }
     //3.Events
     window.addEventListener('scroll', scroll);
+    window.addEventListener('load', scroll);
+
 }
 
 
