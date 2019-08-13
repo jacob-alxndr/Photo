ddf36f5af26a8bb177193d5cfa14e74e4e7f634d
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-13 12:27:51 -0700

    scroll function WIP

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -1,38 +1,37 @@
 //1.Selectors
 import { elements } from "./DOMelements";
 
-if (elements.gallerySet.querySelectorAll('.image-set')) {
+//2.Actions
+if (elements.gallerySet){
     const images = elements.gallerySet.querySelectorAll('.image-set')
-    console.log(images)
-
     const scroll = function () {
-  
         let delay = 0.25
-    
-            images.forEach(image =>{
+        images.forEach(image =>{
             const topTag = image.getBoundingClientRect().top
             const bottomTag = image.getBoundingClientRect().bottom
-      
-            if(topTag < window.innerHeight && bottomTag > 0){
-                image.style.animation = `fadein 1s ${delay}s both`
+            console.log(topTag)
+            if(topTag < window.innerHeight & bottomTag > 0){
+                image.style.animation = `fadein 3s ${delay}s both`
                 delay = delay + 0.2
+                image.style.border = "5px solid green"
             }
-        //    else{
-        //         image.style.opacity = 0
-        //         image.style.animation = ""
-        //    }
+            else{
+                image.style.opacity = 0
+                image.style.animation = ""
+                image.style.border = "5px solid red"
+            }
         })
        
     }
-    
+    //3.Events
     window.addEventListener('scroll', scroll);
 }
 
 
 
-//2.Actions
 
 
 
-//3.Events
+
+
 
