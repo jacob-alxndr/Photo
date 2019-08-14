fe7d98c8532f759a54c441322abd0eaae1cb64a8
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-13 19:43:43 -0700

    playing with grid ideas on ijji page

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -4,24 +4,24 @@ import { elements } from "./DOMelements";
 //2.Actions
 if (elements.gallerySet){
 
-    const images = elements.gallerySet.querySelectorAll('.image-set')
+    const images = elements.gallerySet.querySelectorAll('.image-set img')
 
-    // images.forEach(image =>{
-    //     image.style.opacity = 0;
-    // })
+    images.forEach(image =>{
+        image.style.opacity = 0;
+    })
     
     const scroll = function () {
         // let odd = 2
-        let delay = 0.5
+        let delay = 0.25
         images.forEach(image =>{
             const topTag = image.getBoundingClientRect().top
             const bottomTag = image.getBoundingClientRect().bottom
 
             if(topTag < window.innerHeight && bottomTag > 0){
-                // image.style.animation = `fadeIn 1s ${delay}s both`
+                image.style.animation = `fadeIn 1s ${delay}s both`
                 // odd = odd + 1
                 // if(odd%2 == 0){
-                    // delay = delay + 0.5
+                    delay = delay + 0.25
                 // }
                 console.log(delay)
                 image.style.opacity = 1;
