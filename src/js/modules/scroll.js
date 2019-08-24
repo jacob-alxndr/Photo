5758ecd84f898e20eb264e2af6a6414aca5242e2
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-23 22:13:24 -0700

    responsive contact pg

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -12,14 +12,14 @@ if (elements.gallerySet){
     
     const scroll = function () {
 
-        let delay = 0.15
+        let delay = 0.1
         images.forEach(image =>{
             const topTag = image.getBoundingClientRect().top
             const bottomTag = image.getBoundingClientRect().bottom
                                             // && bottomTag > 0
             if(topTag < window.innerHeight){
                 image.style.animation = `fadeIn 1s ${delay}s both`
-                delay = delay + 0.15
+                delay = delay + 0.1
                 image.style.opacity = 1;
             }
         })
