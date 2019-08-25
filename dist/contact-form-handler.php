7b1943cf4d770eb7efc10842220b4126216114ff
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-24 17:27:47 -0700

    attempt at php form

--- dist/contact-form-handler.php
+++ dist/contact-form-handler.php
@@ -15,7 +15,7 @@ if (isset($_POST["name"]) && isset($_POST["email"])) {
 
     $to = "hellojacobmartinez@gmail.com";
     $headers = "From: $email_from \r\n";
-    $headers .= "Reply-to: $email \r\n";
+    $headers = "Reply-to: $email \r\n";
     
     mail($to,$email_subject,$email_body);
 
