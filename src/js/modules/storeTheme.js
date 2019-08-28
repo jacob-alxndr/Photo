bd314e644e77c72001b1d3815c9e0e44c625706c
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-08-28 15:08:22 -0700

    playing with image scale on gallery pages

--- src/js/modules/storeTheme.js
+++ src/js/modules/storeTheme.js
@@ -15,7 +15,8 @@ function switchTheme(e) {
     if (e.target.checked) {
         elements.htmlTag.setAttribute('data-theme', 'dark-mode')
         localStorage.setItem('theme', 'dark-mode');
-        elements.toggleSwitchUI.style.animation = 'stretchIt .2s ease'
+        // elements.toggleSwitchUILeft.style.animation = 'stretchIt .2s ease'
+        // elements.toggleSwitchUIRight.style.animation = 'stretchIt .2s ease'
     }else {        
         elements.htmlTag.setAttribute('data-theme', 'light-mode')
         localStorage.setItem('theme', 'light-mode');
@@ -23,16 +24,18 @@ function switchTheme(e) {
 }
 
 const addAni = () => {
-    elements.toggleSwitchUI.style.animation = 'stretchIt .2s ease'
+    // elements.toggleSwitchUILeft.style.animation = 'stretchIt .2s ease'
+    // elements.toggleSwitchUIRight.style.animation = 'stretchIt .2s ease'
 }
 
 const removeAni = () =>{
-    elements.toggleSwitchUI.style.animation = ''
+    elements.toggleSwitchUILeft.style.animation = ''
+    elements.toggleSwitchUIRight.style.animation = ''
 }
 
 
 //3.Events
 elements.toggleSwitch.addEventListener('click', addAni);
-elements.toggleSwitchUI.addEventListener('transitionend', removeAni);
+elements.toggleSwitchUILeft.addEventListener('transitionend', removeAni);
 elements.toggleSwitch.addEventListener('change', switchTheme)
-elements.toggleSwitchUI.addEventListener('transitionend', removeAni);
+elements.toggleSwitchUIRight.addEventListener('transitionend', removeAni);
