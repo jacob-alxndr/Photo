a901891d24134c562ee96e5f809b92390069eda3
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-11-04 18:44:03 -0800

    added porfolio js

--- src/js/modules/portfolioHover.js
+++ src/js/modules/portfolioHover.js
@@ -2,12 +2,30 @@
 import { elements } from "./DOMelements";
 
 //2. Actions
-const imgSurface = () => {
+// const imgSurface = () => {
+   
     
-}
-
+// }
 
 //3. Events
-elements.menuItems.forEach(item =>{ 
-    item.addEventListener('mouseover', imgSurface)
+elements.menuItem.forEach(item =>{ 
+    item.addEventListener('mouseover', function (){
+        elements.imgs.forEach(img => {
+            if(img.getAttribute("data-tag") === item.getAttribute("data-tag")){
+                // img.style.border = "5px solid red"
+                img.style.display = "block"
+                img.style.opacity = 1
+                img.style.visibility = "visible";
+                console.log(item.getAttribute("data-tag"))
+            } else{
+                img.style.display = "none"
+                img.style.opacity = 0
+                img.style.visibility = "hidden";
+            }
+        })
+    })
 })
+
+
+
+
