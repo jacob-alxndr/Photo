2fb60bb561b297d975818050bbd10e24c94b2887
Jacob Martinez <jaccob.alxndr@gmail.com>
2019-11-11 07:51:52 -0800

    cleaning up code

--- src/js/modules/storeTheme.js
+++ src/js/modules/storeTheme.js
@@ -23,18 +23,9 @@ function switchTheme(e) {
     }
 }
 
-const addAni = () => {
-    // elements.toggleSwitchUILeft.style.animation = 'stretchIt .2s ease'
-    // elements.toggleSwitchUIRight.style.animation = 'stretchIt .2s ease'
-}
 
-const removeAni = () => {
-    elements.toggleSwitchUILeft.style.animation = ''
-    elements.toggleSwitchUIRight.style.animation = ''
-}
 
 //3.Events
-elements.toggleSwitch.addEventListener('click', addAni)
-elements.toggleSwitchUILeft.addEventListener('transitionend', removeAni)
+
 elements.toggleSwitch.addEventListener('change', switchTheme)
-elements.toggleSwitchUIRight.addEventListener('transitionend', removeAni)
+elements.toggleSwitch.addEventListener('load', switchTheme)
