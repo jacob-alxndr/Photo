532bf5996cf04855466e91ba54f47b6e1f473d2b
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-01-10 14:30:30 -0800

    reduced scripts, began working on accesibility features

--- src/js/modules/hover.js
+++ src/js/modules/hover.js
@@ -8,7 +8,29 @@ import { elements } from './DOMelements';
 const hover = () => {
   if (window.innerWidth > 800) {
     elements.menuItem.forEach(item => {
+      const link = item.querySelector('.menu__link');
+
       item.addEventListener('mouseover', function() {
+        elements.imgs.forEach(img => {
+          if (img.getAttribute('data-tag') === item.getAttribute('data-tag')) {
+            img.style.display = 'block';
+            img.style.opacity = 1;
+            img.style.visibility = 'visible';
+
+            if (window.innerWidth < 800) {
+              img.style.display = 'none';
+              img.style.opacity = 0;
+              img.style.visibility = 'hidden';
+            }
+          } else {
+            img.style.display = 'none';
+            img.style.opacity = 0;
+            img.style.visibility = 'hidden';
+          }
+        });
+      });
+
+      link.addEventListener('focus', function() {
         elements.imgs.forEach(img => {
           if (img.getAttribute('data-tag') === item.getAttribute('data-tag')) {
             img.style.display = 'block';
