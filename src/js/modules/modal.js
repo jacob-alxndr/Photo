1955dd68651ae2934dca4a6e69dda0c2a870ae15
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-01-12 13:15:26 -0800

    keyboard accessibility

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -5,15 +5,30 @@ import { elements } from './DOMelements';
 if (elements.closeModal) {
   function selectImg(e) {
     if (window.innerWidth >= 889) {
-      elements.modalCheck.checked = false;
-      elements.modal.classList.add('open');
-      elements.modalImg.src = e.target.src;
+      if (e.type === 'click') {
+        const link = e.target.closest('a');
+        // link.preventDefault();
+        console.dir(e);
+        elements.modalCheck.checked = false;
+        elements.modal.classList.add('open');
+        elements.modalImg.src = e.target.src;
+      } else if (e.type === 'keydown') {
+        const ent = e.keyCode;
+        console.log(e.keyCode);
+        if (ent === 13) {
+          elements.modalCheck.checked = false;
+          elements.modal.classList.add('open');
+          elements.modalImg.src = e.target.querySelector('img').src;
+        }
+      }
     }
   }
 
   const removeModal = e => {
     if (elements.modal.classList.contains('open')) {
       if (e.type === 'click') {
+        // console.log(e.target);
+
         const isOutside = !e.target.closest('.image-container');
         if (isOutside) {
           elements.modal.classList.remove('open');
@@ -30,6 +45,7 @@ if (elements.closeModal) {
   // 3. Events
   elements.imgs.forEach(img => {
     img.addEventListener('click', selectImg);
+    img.closest('a').addEventListener('keydown', selectImg);
   });
 
   elements.modal.addEventListener('click', removeModal);
